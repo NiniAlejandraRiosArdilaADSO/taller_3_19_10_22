3a8d112d7dda88ec73f88dfafeded07c16b007e7
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-10-13 08:23:21 -0500

    Punto 1

---
+++ main.js
@@ -0,0 +1,14 @@
+addEventListener(`DOMContentLoaded`, ()=>{
+    // Determinar si un número es positivo y menor que 100.
+
+    let numero =(prompt(`por favor esscriba un numero : ` ,95))
+
+    if (numero <= 100 ) {
+        console.log(`su numero es menor que 100`)
+    }
+    else if (numero > 100 ) {
+        console.log(`su numero es mayor a 100`)
+    }
+
+
+})
