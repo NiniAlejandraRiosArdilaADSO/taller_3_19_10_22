d647b593f54a56a2d19f048a30ad18e53fcae6aa
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-10-19 23:38:54 -0500

    Punto 13

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 addEventListener(`DOMContentLoaded`, ()=>{
-    // Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en caso contrario indicar con un mensaje que la operación no es posible realizarla.
-        
-let num1= (prompt(`INGRESE EL PRIMER NUMERO `, 6))
+    // En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+    // de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+    // Los descuentos son los siguientes:
+    // Tipo A 10% de descuento
+    // Tipo B 15% de descuento
+    // // Tipo C 20% de descuento
 
-let num2= (prompt(`INGRESE EL SEGUNDO NUMERO: `, 5))
+console.log(`ELIJA UNA LETRA SEGUN SU MEMBRESIA, SI ES TIPO (a), TIPO(b), TIPO(c) Y (d)NO TIENE MENBRESIA `)
 
-if (num1>num2) {
-    let res=num1-num2
-    console.log(`SU RESPUESTA ES: ${res}`)
-} else {
-    console.log(`NO ES POSIBLE REALIZAR LA OPERACION `)
+let membresia=(prompt(`DIJITE SU TIPO DE MEMBRESIA A, B, C, D: `, `b`))
+
+let num1=(prompt(`INGRESE EL PRECIO DEL HELADO: `,4500))
+
+if (membresia==`a`) {
+    let res= num1*0.10
+    let res2= num1-res
+    console.log (`EL VALOR DEL HELADO ES ${res2} CON EL DESCUENTO DE LA MEMBRESIA TIPO A`)
+} 
+
+else if (membresia == `b`){
+    let res= num1*0.15
+    let res2= num1-res
+    console.log (`EL VALOR DEL HELADO ES ${res2} CON EL DESCUENTO DE LA MEMBRESIA TIPO B`)
 }
-
-
+if (membresia==`c`){
+    let res= num1*0.20
+    let res2= num1-res
+    console.log (`EL VALOR DEL HELADO ES ${res2} CON EL DESCUENTO DE LA MEMBRESIA TIPO C`)
+}else if (membresia==`d`){
+    console.log (`EL VALOR DEL HELADO ES ${num1} SIN NINGUN DESCUENTO )
+}
 
 })
